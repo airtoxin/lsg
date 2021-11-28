93ad8be25ee0d410988dd40e5d0e8dac4fa83cd5
airtoxin <airtoxin@icloud.com>
2021-11-28 09:42:34 +0900

    Styling

--- src/components/models/puzzle/PuzzlePane.tsx
+++ src/components/models/puzzle/PuzzlePane.tsx
@@ -7,14 +7,14 @@ export const PuzzlePane: VoidFunctionComponent<{ puzzle: Puzzle }> = ({
 }) => {
   return (
     <div className="block p-4 m-0 w-full h-full overflow-y-scroll border border-gray-50">
-      <div className="p-4">
+      <div className="pb-4">
         {puzzle.description.split("\n").map((desc) => (
           <div key={desc} className="text-gray-500 text-sm">
             {desc}
           </div>
         ))}
       </div>
-      <div className="p-4">
+      <div className="pb-4">
         <div>Input: {puzzle.input}</div>
       </div>
       {puzzle.tests.map((test) => {
--- src/components/models/solution/SolutionPane.tsx
+++ src/components/models/solution/SolutionPane.tsx
@@ -1,10 +1,15 @@
 import {
   ChangeEvent,
   useCallback,
+  useMemo,
   useState,
   VoidFunctionComponent,
 } from "react";
 import { Puzzle } from "../../../core/puzzles";
+import { lSystem } from "../../../core/LSystem";
+import { pagesPath } from "../../../utils/$path";
+import { format } from "url";
+import { useRouter } from "next/router";
 
 export const SolutionPane: VoidFunctionComponent<{ puzzle: Puzzle }> = ({
   puzzle: p,
@@ -27,10 +32,41 @@ export const SolutionPane: VoidFunctionComponent<{ puzzle: Puzzle }> = ({
     [setPuzzle]
   );
 
+  const puzzleSuccess = useMemo(() => {
+    return puzzle == null
+      ? undefined
+      : puzzle.tests.every(
+          (test) =>
+            test.result != null &&
+            test.resultAnimationText != null &&
+            test.result === test.expect &&
+            test.resultAnimationText === test.expect
+        );
+  }, [puzzle]);
+
+  const handleRunTest = useCallback(() => {
+    setPuzzle((puzzle) => {
+      return puzzle == null
+        ? puzzle
+        : {
+            ...puzzle,
+            tests: puzzle.tests.map((test) => ({
+              ...test,
+              result: lSystem.exec(puzzle.input, puzzle.rules, test.step),
+              resultAnimationText: "",
+            })),
+          };
+    });
+  }, [setPuzzle]);
+  const router = useRouter();
+  const handleReturnToMenu = useCallback(() => {
+    router.push(format(pagesPath.$url()));
+  }, [router]);
+
   return (
     <div className="block p-4 m-0 w-full flex-shrink-0 sm:flex-shrink overflow-scroll border border-gray-50">
       {puzzle.rules.map((rule, i) => (
-        <div key={i} className="p-4 pt-0 flex items-center">
+        <div key={i} className="pb-4 flex items-center">
           <div className="text-center">{rule.from}</div>
           <div className="ml-4 mr-4">=&gt;</div>
           <input
@@ -46,6 +82,17 @@ export const SolutionPane: VoidFunctionComponent<{ puzzle: Puzzle }> = ({
           />
         </div>
       ))}
+      <hr className="pb-4" />
+      <div className="flex flex-col pb-4">
+        <button
+          className="flex justify-center items-center border border-gray-50 font-bold rounded p-0.5 bg-gray-900 hover:bg-gray-700 active:bg-gray-500"
+          onClick={() =>
+            puzzleSuccess ? handleReturnToMenu() : handleRunTest()
+          }
+        >
+          {puzzleSuccess ? "Back to menu" : "Run test"}
+        </button>
+      </div>
     </div>
   );
 };
