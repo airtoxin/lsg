0c7ad199c63921084f9142d959feeaaef83bab8e
airtoxin <airtoxin@icloud.com>
2021-11-28 10:30:45 +0900

    Fix timing

--- src/components/models/TestResult.tsx
+++ src/components/models/TestResult.tsx
@@ -4,7 +4,9 @@ import { Test } from "../../core/puzzles";
 export const TestResult: VoidFunctionComponent<{ test: Test }> = ({ test }) => {
   const testSuccess = useMemo(
     () =>
-      test.result == null || test.resultAnimationText == null
+      test.result == null ||
+      test.resultAnimationText == null ||
+      test.result !== test.resultAnimationText
         ? undefined
         : test.result === test.expect,
     [test.expect, test.result, test.resultAnimationText]
@@ -14,19 +16,16 @@ export const TestResult: VoidFunctionComponent<{ test: Test }> = ({ test }) => {
     <div>
       <div>Step {test.step}</div>
       <div>Expect: {test.expect}</div>
-      <div>
-        <span
-          className={
-            testSuccess == null
-              ? ""
-              : testSuccess
-              ? "text-green-300"
-              : "text-red-300"
-          }
-        >
-          Result
-        </span>
-        : {test.resultAnimationText ?? ""}
+      <div
+        className={
+          testSuccess == null
+            ? ""
+            : testSuccess
+            ? "text-green-300"
+            : "text-red-300"
+        }
+      >
+        Result: {test.resultAnimationText ?? ""}
       </div>
     </div>
   );
