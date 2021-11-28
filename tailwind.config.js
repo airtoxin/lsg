776a93f3f796e24ad1a83956529428f8e382253e
airtoxin <airtoxin@icloud.com>
2021-11-28 15:42:11 +0900

    Enable jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
-  },
-  variants: {
-    extend: {
-      backgroundColor: ["active"],
-    },
   },
   plugins: [],
 };
